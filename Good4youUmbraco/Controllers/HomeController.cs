b803bb25a7bd812db9d8f373785228abe44e4bbe
Kasia Kordys <kasia.kordys@gmail.com>
2020-05-12 21:24:47 +0100

    add home controller and featured partial view

---
+++ Good4youUmbraco/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Good4youUmbraco.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        public ActionResult RenderFeatured()
+        {
+            return PartialView("~/Views/Partials/Home/Featured.cshtml");
+        }
+    }
+}
